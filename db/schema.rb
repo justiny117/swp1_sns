998cf2849d34876e97c5506faa7e1bf8bb9f6c1b
justiny117 <justiny117@gmail.com>
2015-12-01 09:13:44 +0000

    email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119060127) do
+ActiveRecord::Schema.define(version: 20151126063421) do
 
   create_table "agreecs", force: :cascade do |t|
     t.integer  "comment_id"
@@ -71,8 +71,12 @@ ActiveRecord::Schema.define(version: 20151119060127) do
     t.datetime "updated_at",                             null: false
     t.string   "nickname"
     t.boolean  "is_female",              default: false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
